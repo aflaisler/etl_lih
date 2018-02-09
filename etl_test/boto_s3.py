17983a8193fe4b6a3b51cf23ea0f6ec5465a92fb
Aymeric Flaisler <aflaisler@gmail.com>
2018-02-09 14:45:25 +0000

    access to s3

---
+++ etl_test/boto_s3.py
@@ -0,0 +1,21 @@
+from boto.s3.connection import S3Connection
+import json
+
+def s3_access_keys(credentials_path="client/credentials.json"):
+    # get the socialbakers api secret key use the absolute path in production
+    try:
+        filepath = os.path.dirname(os.path.realpath(__file__)) + "/" + credentials_path
+    except:
+        filepath = credentials_path
+    sb_credentials = json.load(open(filepath))
+    key = sb_credentials['s3accessKeys']['AWS_ACCESS_KEY_ID']
+    secret = sb_credentials['s3accessKeys']['AWS_SECRET_ACCESS_KEY']
+    return key, secret
+
+
+key, secret = s3_access_keys(credentials_path="client/credentials.json")
+conn = S3Connection(key, secret)
+
+bucket = conn.get_bucket('ai-sony-automation')
+for obj in bucket.get_all_keys():
+    print(obj.key)
